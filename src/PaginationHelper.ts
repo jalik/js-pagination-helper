49a4df64d32d21f1d5b4d8ab496237830a8c447c
Karl Stein <karl.stein.pro@gmail.com>
2024-02-27 09:40:20 -1000

    Migrated to TypeScript Replaced eslint-config-airbnb-base by eslint-config-standard

---
+++ src/PaginationHelper.ts
@@ -0,0 +1,224 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2020 Karl STEIN
+ */
+
+export type PaginationHelperOptions = {
+  limit: number
+  offset?: number
+  // todo add minPage
+  // todo rename to number
+  page?: number
+  // todo rename to totalElements
+  total: number
+}
+
+// todo rename to OffsetPagination
+class PaginationHelper {
+  private limit!: number
+  private offset!: number
+  private total!: number
+
+  constructor (options: PaginationHelperOptions) {
+    this.setLimit(options.limit)
+    this.setTotal(options.total)
+
+    // Set the offset using the page number
+    if (options.page != null) {
+      this.setPage(options.page)
+    } else if (options.offset != null) {
+      this.setOffset(options.offset)
+    }
+  }
+
+  /**
+   * Returns the closest valid page number
+   * @param page
+   */
+  getClosestPage (page: number): number {
+    let closest = page
+    const count = this.getPageCount()
+
+    if (page > count) {
+      closest = count
+    } else if (closest < 1) {
+      closest = 1
+    }
+    return closest
+  }
+
+  /**
+   * Compares with another pagination to see if they are equals.
+   * @param pagination
+   * todo rename to equals
+   */
+  equal (pagination: PaginationHelper): boolean {
+    return this.getLimit() === pagination.getLimit() &&
+      this.getOffset() === pagination.getOffset() &&
+      this.getTotal() === pagination.getTotal()
+  }
+
+  /**
+   * Returns the last page.
+   */
+  getLastPage (): number {
+    return this.getPageCount()
+  }
+
+  /**
+   * Returns the number of elements per page.
+   */
+  getLimit (): number {
+    return this.limit
+  }
+
+  /**
+   * Returns the next page.
+   */
+  getNextPage (): number {
+    return this.getPage() < this.getPageCount()
+      ? this.getPage() + 1
+      : this.getLastPage()
+  }
+
+  /**
+   * Returns the page offset.
+   */
+  getOffset (): number {
+    return this.offset
+  }
+
+  /**
+   * Returns the offset from a page.
+   * @param page
+   */
+  getOffsetFromPage (page: number): number {
+    return this.limit * (page - 1)
+  }
+
+  /**
+   * Returns the page number.
+   */
+  getPage (): number {
+    return this.getPageFromOffset(this.offset)
+  }
+
+  /**
+   * Returns the number of pages.
+   * todo rename to getTotalPages()
+   */
+  getPageCount (): number {
+    return this.total > 0 && this.limit > 0
+      ? Math.ceil(this.total / this.limit)
+      : 1
+  }
+
+  /**
+   * Returns a page from an offset.
+   * @param offset
+   */
+  getPageFromOffset (offset: number): number {
+    return offset > 0 && this.limit > 0
+      ? Math.round(offset / this.limit) + 1
+      : 1
+  }
+
+  /**
+   * Returns the previous page.
+   */
+  getPreviousPage (): number {
+    return Math.max(1, this.getPage() - 1)
+  }
+
+  /**
+   * Returns the number of elements.
+   * todo rename to getTotalElements()
+   */
+  getTotal (): number | null {
+    return this.total
+  }
+
+  /**
+   * Checks if there is a page after.
+   */
+  hasNext (): boolean {
+    return this.getPage() < this.getNextPage()
+  }
+
+  /**
+   * Checks if there is a page before.
+   */
+  hasPrevious (): boolean {
+    return this.getPage() > this.getPreviousPage()
+  }
+
+  /**
+   * Checks if the page is valid.
+   * @param page
+   */
+  isPageValid (page: number): boolean {
+    return page > 0 && page <= this.getPageCount()
+  }
+
+  /**
+   * Moves to the next page.
+   */
+  next () {
+    if (this.hasNext()) {
+      this.offset += this.limit
+    }
+    return this
+  }
+
+  /**
+   * Moves to the previous page.
+   */
+  previous () {
+    if (this.hasPrevious()) {
+      this.offset -= this.limit
+    } else {
+      this.offset = 0
+    }
+    return this
+  }
+
+  /**
+   * Sets the number of elements per page.
+   * @param limit
+   */
+  setLimit (limit: number) {
+    this.limit = Math.max(0, Math.round(limit))
+    return this
+  }
+
+  /**
+   * Sets the page offset.
+   * @param offset
+   */
+  setOffset (offset: number) {
+    this.offset = Math.max(0, Math.round(offset))
+    return this
+  }
+
+  /**
+   * Sets the page number and update the offset accordingly.
+   * @param page
+   */
+  setPage (page: number) {
+    this.setOffset(this.getOffsetFromPage(page))
+    return this
+  }
+
+  /**
+   * Sets the total number of elements.
+   * todo rename to setTotalElements()
+   * @param total
+   */
+  setTotal (total: number) {
+    this.total = Math.max(0, Math.round(total))
+    return this
+  }
+}
+
+// todo use named export
+export default PaginationHelper
---
+++ test/PaginationHelper.test.ts
@@ -0,0 +1,230 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2020 Karl STEIN
+ */
+
+import { describe, expect, it } from '@jest/globals'
+import PaginationHelper from '../src/PaginationHelper'
+
+describe('PaginationHelper', () => {
+  it('should be importable from package', () => {
+    expect(typeof PaginationHelper).toEqual('function')
+  })
+})
+
+describe('equal(pagination)', () => {
+  const p1 = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+  const p2 = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+
+  it('should return true if paginations limit, offset and total are equals', () => {
+    expect(p1.equal(p2)).toEqual(true)
+  })
+
+  it('should return true if paginations limit are differents', () => {
+    const p3 = new PaginationHelper({ limit: 20, page: 1, total: 100 })
+    expect(p1.equal(p3)).toEqual(false)
+  })
+
+  it('should return true if paginations page are differents', () => {
+    const p4 = new PaginationHelper({ limit: 10, page: 2, total: 100 })
+    expect(p1.equal(p4)).toEqual(false)
+  })
+
+  it('should return true if paginations total are differents', () => {
+    const p5 = new PaginationHelper({ limit: 10, page: 1, total: 50 })
+    expect(p1.equal(p5)).toEqual(false)
+  })
+})
+
+describe('getClosestPage(page)', () => {
+  const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+
+  it('should return 1 if page is 0', () => {
+    expect(p.getClosestPage(0)).toEqual(1)
+  })
+
+  it('should return the last page if page is above page count', () => {
+    expect(p.getClosestPage(25)).toEqual(10)
+  })
+
+  it('should return the same page if there nothing to fix', () => {
+    expect(p.getClosestPage(5)).toEqual(5)
+  })
+})
+
+describe('getLastPage()', () => {
+  const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+
+  it('should return the last page', () => {
+    expect(p.getLastPage()).toEqual(10)
+  })
+})
+
+describe('getLimit()', () => {
+  const p = new PaginationHelper({ limit: 5, page: 1, total: 100 })
+
+  it('should return the limit per page', () => {
+    expect(p.getLimit()).toEqual(5)
+  })
+})
+
+describe('getNextPage()', () => {
+  it('should return the next page if there is a page after', () => {
+    const p = new PaginationHelper({ limit: 10, page: 5, total: 100 })
+    expect(p.getNextPage()).toEqual(6)
+  })
+
+  it('should return the last page if there is no page after', () => {
+    const p = new PaginationHelper({ limit: 10, page: 10, total: 100 })
+    expect(p.getNextPage()).toEqual(10)
+  })
+})
+
+describe('getOffset()', () => {
+  it('should return the offset', () => {
+    const p1 = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+    const p2 = new PaginationHelper({ limit: 10, page: 5, total: 100 })
+    expect(p1.getOffset()).toEqual(0)
+    expect(p2.getOffset()).toEqual(40)
+  })
+})
+
+describe('getPage()', () => {
+  const p = new PaginationHelper({ limit: 10, page: 3, total: 100 })
+
+  it('should return the current page', () => {
+    expect(p.getPage()).toEqual(3)
+  })
+})
+
+describe('getPageCount()', () => {
+  const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+
+  it('should return the page count', () => {
+    expect(p.getPageCount()).toEqual(10)
+  })
+})
+
+describe('getPreviousPage()', () => {
+  it('should return the previous page if there is a page before', () => {
+    const p = new PaginationHelper({ limit: 10, page: 5, total: 100 })
+    expect(p.getPreviousPage()).toEqual(4)
+  })
+
+  it('should return the first page if there is no page before', () => {
+    const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+    expect(p.getPreviousPage()).toEqual(1)
+  })
+})
+
+describe('getTotal()', () => {
+  const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+
+  it('should return the total', () => {
+    expect(p.getTotal()).toEqual(100)
+  })
+})
+
+describe('hasNext()', () => {
+  it('should return true if there is a page after', () => {
+    const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+    expect(p.hasNext()).toEqual(true)
+  })
+
+  it('should return false if there is no page after', () => {
+    const p = new PaginationHelper({ limit: 10, page: 10, total: 100 })
+    expect(p.hasNext()).toEqual(false)
+  })
+})
+
+describe('hasPrevious()', () => {
+  it('should return true if there is a page before', () => {
+    const p = new PaginationHelper({ limit: 10, page: 5, total: 100 })
+    expect(p.hasPrevious()).toEqual(true)
+  })
+
+  it('should return false if there is no page before', () => {
+    const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+    expect(p.hasPrevious()).toEqual(false)
+  })
+})
+
+describe('isPageValid(page)', () => {
+  const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+
+  it('should return false if page is below 1', () => {
+    expect(p.isPageValid(0)).toEqual(false)
+  })
+
+  it('should return true if page is in valid range', () => {
+    expect(p.isPageValid(1)).toEqual(true)
+  })
+
+  it('should return false if page is above page count', () => {
+    expect(p.isPageValid(300)).toEqual(false)
+  })
+})
+
+describe('next()', () => {
+  it('should increase offset if there is a page after', () => {
+    const p = new PaginationHelper({ limit: 10, page: 5, total: 100 })
+    p.next()
+    expect(p.getOffset()).toEqual(50)
+  })
+
+  it('should not increase offset if there is no page after', () => {
+    const p = new PaginationHelper({ limit: 10, page: 10, total: 100 })
+    p.next()
+    expect(p.getOffset()).toEqual(90)
+  })
+})
+
+describe('previous()', () => {
+  it('should decrease offset if there is a page before', () => {
+    const p = new PaginationHelper({ limit: 10, page: 2, total: 100 })
+    p.previous()
+    expect(p.getOffset()).toEqual(0)
+  })
+
+  it('should not decrease offset if there is no page before', () => {
+    const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+    p.previous()
+    expect(p.getOffset()).toEqual(0)
+  })
+})
+
+describe('setLimit(limit)', () => {
+  it('should change the limit per page', () => {
+    const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+    p.setLimit(25)
+    expect(p.getLimit()).toEqual(25)
+  })
+
+  it('should change the limit to 0 if limit is negative', () => {
+    const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+    p.setLimit(-1)
+    expect(p.getLimit()).toEqual(0)
+  })
+})
+
+describe('setOffset(offset)', () => {
+  it('should change the offset', () => {
+    const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+    p.setOffset(25)
+    expect(p.getOffset()).toEqual(25)
+  })
+
+  it('should change the offset to 0 if offset is negative', () => {
+    const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+    p.setLimit(-1)
+    expect(p.getOffset()).toEqual(0)
+  })
+})
+
+describe('setPage(page)', () => {
+  it('should change the page', () => {
+    const p = new PaginationHelper({ limit: 10, page: 1, total: 100 })
+    p.setPage(5)
+    expect(p.getPage()).toEqual(5)
+  })
+})
